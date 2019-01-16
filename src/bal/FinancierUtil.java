4730ee9c592da3459ff027a82e4a312b6193490a
Clement <clement.marescaux@gmail.com>
2019-01-16 13:58:21 +0100

    modified FinancierUtil

--- src/bal/FinancierUtil.java
+++ src/bal/FinancierUtil.java
@@ -1,5 +1,44 @@
 package bal;
 
-public class FinancierUtil {
+import java.util.ArrayList;
+import dal.*;
+
+public final class FinancierUtil {
+
+	public static double[] calculBilanMois (int annee, String mois, ArrayList<Depense> listDepense, ArrayList<Recette> listRecette) {
+		double totalRecetteMois = 0;
+		double totalDepenseMois = 0;
+		
+		for (Recette recette : listRecette) {
+			totalRecetteMois = totalRecetteMois + recette.getCount();
+		}
+		
+		for (Depense depense : listDepense) {
+			totalDepenseMois = totalDepenseMois + depense.getCount();
+		}
+		
+		double bilanMois = totalRecetteMois - totalDepenseMois;
+		
+		return new double[] { totalDepenseMois, totalRecetteMois, bilanMois };		
+	}
+	
+	public static double[] calculBilanAnnee (ArrayList<Depense> listDepense, ArrayList<Recette> listRecette, int annee) {
+		double totalRecetteAnnee = 0;
+		double totalDepenseAnnee = 0;
+		
+		for (Recette recette : listRecette) {
+			totalRecetteAnnee = totalRecetteAnnee + recette.getCount();
+		}
+		
+		for (Depense depense : listDepense) {
+			totalDepenseAnnee = totalDepenseAnnee + depense.getCount();
+		}
+		
+		double bilanAnnee = totalRecetteAnnee - totalDepenseAnnee;
+		
+		return new double[] { totalDepenseAnnee, totalRecetteAnnee, bilanAnnee };
+
+	}
+	
 
 }
