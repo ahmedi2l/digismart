40c13f8f863d939344f5a0562315c953f9c86e0b
Ahmed Abdallah <aabdallah.m2i@gmail.com>
2019-01-16 14:52:02 +0100

    Implementation de TypeRessourceUtil

--- src/dal/BDD.java
+++ src/dal/BDD.java
@@ -7,7 +7,7 @@ public class BDD {
 
 	public static ArrayList<Depense> listDepense = ListeDepenseGenerator.createDepenseDB(2015,3);
 	public static ArrayList<Recette> listRecette;
-	public static ArrayList<String> listType;
+	public static ArrayList<String> listType = new ArrayList<>();
 	public static ArrayList<Professeur> professeurs;
 	
 }
